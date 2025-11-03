1de1b87c65f2a03ba2caed358d9c2511ddf00606
rehan6025 <rehanthakur6025@gmail.com>
2025-11-03 19:27:53 +0530

    added quick sort

--- src/components/SortingVisualizer.tsx
+++ src/components/SortingVisualizer.tsx
@@ -33,7 +33,7 @@ const SortingVisualizer = () => {
 
     const generateArray = (size: number): number[] => {
         return Array.from({ length: size }, () =>
-            Math.ceil(Math.random() * (300 - 5) + 5)
+            Math.ceil(Math.random() * (400 - 5) + 5)
         );
     };
 
@@ -139,8 +139,11 @@ const SortingVisualizer = () => {
 
         const mid = Math.floor((start + end) / 2);
 
+        if (cancelRef.current) return array;
         await _mergeSort(array, temp, start, mid);
+        if (cancelRef.current) return array;
         await _mergeSort(array, temp, mid + 1, end);
+        if (cancelRef.current) return array;
 
         let i = start;
         let j = mid + 1;
@@ -155,6 +158,7 @@ const SortingVisualizer = () => {
             await new Promise((resolve) => {
                 setTimeout(resolve, speed);
             });
+            if (cancelRef.current) return array;
 
             setActive([i, j]);
         }
@@ -175,9 +179,59 @@ const SortingVisualizer = () => {
             await new Promise((resolve) => {
                 setTimeout(resolve, speed);
             });
+            if (cancelRef.current) return array;
         }
 
         setActive([]);
+    }
+
+    async function quickSort(array: number[], low: number, high: number) {
+        if (low < high) {
+            const pivotInd = await partition(array, low, high);
+
+            await new Promise((resolve) => setTimeout(resolve, speed));
+            if (cancelRef.current) return;
+
+            setActive([]);
+            setMinim(-1);
+
+            await quickSort(array, low, pivotInd - 1);
+
+            await new Promise((resolve) => setTimeout(resolve, speed));
+            if (cancelRef.current) return;
+
+            setActive([]);
+            setMinim(-1);
+
+            await quickSort(array, pivotInd + 1, high);
+        }
+    }
+
+    async function partition(array: number[], low: number, high: number) {
+        const pivot = array[high];
+        let i = low - 1;
+        setMinim(high);
+
+        for (let j = low; j < high; j++) {
+            setActive([i, j]);
+            await new Promise((resolve) => setTimeout(resolve, speed));
+            if (cancelRef.current) return high;
+
+            if (array[j] <= pivot) {
+                i++;
+                [array[i], array[j]] = [array[j], array[i]];
+                setArray([...array]);
+            }
+        }
+
+        await new Promise((resolve) => setTimeout(resolve, speed));
+        [array[i + 1], array[high]] = [array[high], array[i + 1]];
+        setArray([...array]);
+
+        await new Promise((resolve) => setTimeout(resolve, speed));
+        if (cancelRef.current) return high;
+
+        return i + 1;
     }
 
     // useEffect(() => {
@@ -216,100 +270,136 @@ const SortingVisualizer = () => {
     };
 
     return (
-        <div className="flex flex-col items-center w-full h-[80vh] overflow-hidden p-2">
+        <div className="flex flex-col justify-between items-center w-full h-[80vh] overflow-hidden p-2">
             <div className="flex items-end justify-center w-full h-full bg-light-background rounded-md overflow-hidden">
-                {array.map((size, index) => (
-                    <span
-                        key={index}
-                        style={{ height: `${size}px` }}
-                        className={`${
-                            sortedEle.includes(index)
-                                ? "bg-orange-400"
-                                : minim === index
-                                ? "bg-amber-50"
-                                : active.includes(index)
-                                ? "bg-green-400"
-                                : "bg-teal-500"
-                        } w-1 ml-1 transition-all duration-75 `}
-                    ></span>
-                ))}
+                {array.length > 40 &&
+                    array.map((size, index) => (
+                        <span
+                            key={index}
+                            style={{ height: `${size}px` }}
+                            className={`${
+                                sortedEle.includes(index)
+                                    ? "bg-orange-400"
+                                    : minim === index
+                                    ? "bg-amber-50"
+                                    : active.includes(index)
+                                    ? "bg-green-400"
+                                    : "bg-teal-500"
+                            } w-1 ml-1 transition-all duration-75 `}
+                        ></span>
+                    ))}
+
+                {array.length <= 40 &&
+                    array.map((size, index) => (
+                        <span
+                            key={index}
+                            style={{ height: `${size}px` }}
+                            className={`${
+                                sortedEle.includes(index)
+                                    ? "bg-orange-400"
+                                    : minim === index
+                                    ? "bg-amber-50"
+                                    : active.includes(index)
+                                    ? "bg-green-400"
+                                    : "bg-teal-500"
+                            } w-7 ml-1 transition-all duration-75 text-sm text-center`}
+                        >
+                            {size}
+                        </span>
+                    ))}
             </div>
-            <input
-                type="range"
-                min={4}
-                max={150}
-                id="size"
-                onChange={(e) => {
-                    setSize(Number(e.target.value));
-                }}
-                disabled={isSorting}
-            />
-            <label htmlFor="size" className="text-light-text">
-                Size
-            </label>
-            <input
-                type="range"
-                min={0}
-                max={350}
-                id="speed"
-                onChange={(e) => {
-                    setSpeed(MAX_SPEED - Number(e.target.value));
-                }}
-                disabled={isSorting}
-            />
-            <label htmlFor="speed" className="text-light-text">
-                Speed
-            </label>
-            <button
-                className="px-4 py-2 bg-light-background text-light-text rounded"
-                onClick={generateNewArray}
-            >
-                Randomize
-            </button>
-            <button
-                className="px-4 py-2 bg-light-background text-light-text rounded"
-                onClick={async () => {
-                    cancelRef.current = false;
-                    setIsSorting(true);
+            <div className="flex flex-wrap items-center justify-center space-x-2">
+                <input
+                    type="range"
+                    min={4}
+                    max={150}
+                    id="size"
+                    onChange={(e) => {
+                        setSize(Number(e.target.value));
+                    }}
+                    disabled={isSorting}
+                />
+                <label htmlFor="size" className="text-light-text">
+                    Size
+                </label>
+                <input
+                    type="range"
+                    min={0}
+                    max={350}
+                    id="speed"
+                    onChange={(e) => {
+                        setSpeed(MAX_SPEED - Number(e.target.value));
+                    }}
+                    disabled={isSorting}
+                />
+                <label htmlFor="speed" className="text-light-text">
+                    Speed
+                </label>
+                <button
+                    className="px-4 py-2 bg-light-background text-light-text rounded"
+                    onClick={generateNewArray}
+                >
+                    Randomize
+                </button>
+                <button
+                    className="px-4 py-2 bg-light-background text-light-text rounded"
+                    onClick={async () => {
+                        cancelRef.current = false;
+                        setIsSorting(true);
 
-                    await bubbleSort(array);
+                        await bubbleSort(array);
 
-                    setIsSorting(false);
-                }}
-                disabled={isSorting}
-            >
-                Bubble Sort
-            </button>
-            {/* <button
-                onClick={() => {
-                    setFrames(bubbleSortFrames(array));
-                }}  
-            >
-                new bubble sort
-            </button> */}
+                        setIsSorting(false);
+                    }}
+                    disabled={isSorting}
+                >
+                    Bubble Sort
+                </button>
+                {/* <button
+                        onClick={() => {
+                            setFrames(bubbleSortFrames(array));
+                        }}  
+                    >
+                        new bubble sort
+                    </button> */}
 
-            <button
-                className="px-4 py-2 bg-light-background text-light-text rounded"
-                onClick={async () => {
-                    cancelRef.current = false;
-                    setIsSorting(true);
-                    await selectionSort(array);
-                    setIsSorting(false);
-                }}
-            >
-                Selection Sort
-            </button>
-            <button
-                className="px-4 py-2 bg-light-background text-light-text rounded"
-                onClick={async () => {
-                    cancelRef.current = false;
-                    setIsSorting(true);
-                    await mergeSort(array);
-                    setIsSorting(false);
-                }}
-            >
-                Merge Sort
-            </button>
+                <button
+                    className="px-4 py-2 bg-light-background text-light-text rounded"
+                    onClick={async () => {
+                        cancelRef.current = false;
+                        setIsSorting(true);
+                        await selectionSort(array);
+                        setIsSorting(false);
+                    }}
+                    disabled={isSorting}
+                >
+                    Selection Sort
+                </button>
+                <button
+                    className="px-4 py-2 bg-light-background text-light-text rounded"
+                    onClick={async () => {
+                        cancelRef.current = false;
+                        setIsSorting(true);
+                        await mergeSort(array);
+                        setIsSorting(false);
+                    }}
+                    disabled={isSorting}
+                >
+                    Merge Sort
+                </button>
+                <button
+                    className="px-4 py-2 bg-light-background text-light-text rounded"
+                    onClick={async () => {
+                        cancelRef.current = false;
+                        setIsSorting(true);
+                        await quickSort(array, 0, array.length - 1);
+                        setIsSorting(false);
+                    }}
+                    disabled={isSorting}
+                >
+                    Quick Sort
+                </button>
+            </div>
         </div>
     );
 };
